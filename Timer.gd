6b25687124146cd24ee163990ea92a1c3d2b105d
Rileyschweiss <112006715+Rileyschweiss@users.noreply.github.com>
2022-09-16 19:57:10 -0400

    Completes Ex
    02c

--- Timer.gd
+++ Timer.gd
@@ -30,3 +30,23 @@ func _on_Area2D_body_entered(body):
 
 func _on_Timer_timeout():
 	queue_free()
+
+
+ENEMY 
+extends KinematicBody2D
+var initial_position = Vector2.ZERO
+var direction = Vector2(1.5,0)
+
+var health = 1
+
+var Effects = null
+onready var Bullet = load("res://Enemy/Bullet.tscn")
+onready var Explosion =load("res://Effects/Explosion.tscn")
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	position += direction
+	if position.x >= 1200:
+		queue_free()
